f08e8253034010563583a025e73dac59c5f65e72
Kate Hudson <katehudsondesign@gmail.com>
2012-10-05 12:03:28 -0400

    added tooltips and stuff

--- webmaker-nav.js
+++ webmaker-nav.js
@@ -37,18 +37,39 @@ document.addEventListener( "DOMContentLoaded", function(){
     el.classList.add( BUTTON_ACTIVE_CLASS );
   };
 
+  // This is just a demo to show login states
   function loginDemo() {
     var login = document.querySelector( ".login-join" ),
-        user = document.querySelector( ".user" );
+        logout = document.querySelector( ".logout-btn" );
 
     login.addEventListener( "click", function( e ) {
-      this.classList.add( "hidden" );
-      user.classList.remove( "hidden" );
+      document.body.classList.add( "webmaker-logged-in" );
+      document.body.classList.remove( "webmaker-not-logged-in" );
+    }, false );
+
+    logout.addEventListener( "click", function( e ) {
+      document.body.classList.remove( "webmaker-logged-in" );
+      document.body.classList.add( "webmaker-not-logged-in" );
+    }, false );
+
+  }
+
+  // This toggles the user info
+  function toggleUserInfo( selector ) {
+    var el = document.querySelector( selector ),
+        tooltip = el.querySelector( ".tooltip" );
+
+    tooltip.addEventListener( "click", function( e ) {
+      e.stopPropagation();
+    }, false );
+    
+    el.addEventListener( "click", function( e ) {
+      tooltip.classList.toggle( "tooltip-no-transition-on" );
     }, false );
   }
 
   webmakerPrimaryNav.addEventListener( "click", webmakerTabSetup, false );
-
   loginDemo();
+  toggleUserInfo( ".user" );
 
 }, false);
